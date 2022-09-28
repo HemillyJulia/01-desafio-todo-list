e4ac3de8137dc7bd5da97bcc731db369c50f2f01
HemillyJulia <hemillyjulia@hotmail.com>
2022-09-27 23:26:40 -0400

    Iniciando o todo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Header } from "./Components/Header"
+import { Newtask } from "./Components/Newtask"
+
+
+function App() {
+ 
+
+  return (
+    <div >
+      <Header/>
+     
+      <h1>todo</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from'./Header.module.css'
+import Logo from'../assets/Logo.png'
+
+
+export function Header () {
+    return(
+        <header className={styles.header}>
+        <img src={Logo} att="Logotipo do todo"/>   
+        </header>
+      
+    )
+}
