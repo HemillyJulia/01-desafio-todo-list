bb9fef231bc2ed3259c588576746bc933f2fe3e0
HemillyJulia <hemillyjulia@hotmail.com>
2022-09-30 22:18:18 -0400

    Console.log ao clicar no botão e ao digitar algo no input.

--- src/Components/Newtask.jsx
+++ src/Components/Newtask.jsx
@@ -1,13 +1,23 @@
-import styles from './Newtask.module.css'
+import styles from "./Newtask.module.css";
 import plus from "../assets/plus.png";
+import { useState } from "react";
 
-export function Newtask (){
-    return (
-        <div className={styles.newtask}>
-        <input placeholder="Adicione uma nova tarefa"></input> <button>Criar <img src={plus} alt="Adicionar" /></button>
-        
-        </div>
-    )
+export function Newtask() {
+  const [inputTasks, setInputTasks] = useState("");
+
+  function createNewTask(e) {
+    e.preventDefault();
+    console.log("Você clicou em criar.");
+
+    console.log(inputTasks)
+  }
+
+  return (
+    <div className={styles.newtask}>
+      <input onChange={ (event) => {setInputTasks(event.target.value) }} value={inputTasks} placeholder="Adicione uma nova tarefa"></input>
+      <button onClick={createNewTask} >
+        Criar <img src={plus} alt="Adicionar" />
+      </button>
+    </div>
+  );
 }
-
-
