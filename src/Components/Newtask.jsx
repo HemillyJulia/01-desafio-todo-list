41e7f0b4463d1e00781f835dc81d18d9325fb2a8
HemillyJulia <hemillyjulia@hotmail.com>
2022-10-02 22:52:26 -0400

    Update Newtask.jsx

--- src/Components/Newtask.jsx
+++ src/Components/Newtask.jsx
@@ -5,17 +5,26 @@ import { useState } from "react";
 export function Newtask() {
   const [inputTasks, setInputTasks] = useState("");
 
-  function createNewTask(e) {
-    e.preventDefault();
-    console.log("Você clicou em criar.");
-
-    console.log(inputTasks)
+  function createNewTask() {
+    //Se o novo valor do meu input for maior que 0 faz o console.log do que estiver escrito
+    //Se não, dispara um alerta na tela.
+    if (inputTasks.length > 0) {
+      console.log("Você clicou em criar.");
+    } else {
+      window.alert("Crie uma tarefa válida!");
+    }
   }
 
   return (
     <div className={styles.newtask}>
-      <input onChange={ (event) => {setInputTasks(event.target.value) }} value={inputTasks} placeholder="Adicione uma nova tarefa"></input>
-      <button onClick={createNewTask} >
+      <input
+        onChange={(event) => {
+          setInputTasks(event.target.value);
+        }}
+        value={inputTasks}
+        placeholder="Adicione uma nova tarefa"
+      ></input>
+      <button onClick={createNewTask}>
         Criar <img src={plus} alt="Adicionar" />
       </button>
     </div>
